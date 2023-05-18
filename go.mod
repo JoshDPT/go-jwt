fa116936e805f2f4e43883b10a287afc4624aa2b
JoshDPT <jjewcu@gmail.com>
2023-05-18 17:02:55 -0400

    add jwt protected routes

--- go.mod
+++ go.mod
@@ -8,4 +8,4 @@ require (
 	github.com/gorilla/mux v1.8.0
 )
 
-require github.com/mattn/go-sqlite3 v1.14.16 // indirect
+require github.com/mattn/go-sqlite3 v1.14.16
--- jwt-api.go
+++ jwt-api.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"fmt"
 
 	"github.com/dgrijalva/jwt-go"
 	_ "github.com/mattn/go-sqlite3"
@@ -25,14 +26,18 @@ func main() {
 	// Connect to the database
 	connectDB()
 
-	router := mux.NewRouter()
+	mainRouter := mux.NewRouter()
 
-	// API endpoint for user authentication
-	router.HandleFunc("/login", login).Methods("POST")
-	router.HandleFunc("/users", createUser).Methods("POST")
+	// Unprotected login route to get JWT token
+	subRouter1 := mainRouter.PathPrefix("/api/v1").Subrouter()
+	subRouter1.HandleFunc("/login", login).Methods("POST")
+	
+	// JWT protected routers
+	subRouter2 := mainRouter.PathPrefix("/api/v2").Subrouter()
+	subRouter2.Use(authMiddleware)
+	subRouter2.HandleFunc("/users", createUser).Methods("POST")
 
-
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", mainRouter))
 }
 
 func connectDB() {
@@ -42,6 +47,7 @@ func connectDB() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Opened database file")
 
 	// Create the users table if it doesn't exist
 	createTable := `
@@ -89,6 +95,7 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Return a success response
+	fmt.Println("Success in adding user")
 	w.WriteHeader(http.StatusCreated)
 }
 
@@ -136,6 +143,61 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Return the signed JWT token
+	fmt.Println("Success in logging on, returned JWT")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"token": signedToken})
 }
+
+
+func validateToken(tokenString string) (bool, error) {
+	// Parse the JWT token
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+
+		// Validate the signing method of the token
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// Provide the secret key used to sign the token
+		// Replace "your-secret-key" with your actual secret key
+		secretKey := []byte("your-secret-key")
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return false, err
+	}
+
+	// Check if the token is valid
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Get the authorization header from the request
+		tokenString := r.Header.Get("Authorization")
+
+		// Check if the token is provided
+		if tokenString == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		// Validate the token
+		isValid, err := validateToken(tokenString)
+		if err != nil || !isValid {
+			fmt.Println("Token is not valid")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		// Token is valid, proceed to the next handler
+		fmt.Println("Token is valid")
+		next.ServeHTTP(w, r)
+	})
+}
+
